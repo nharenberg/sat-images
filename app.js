f06874e4ede2fdfcbab39d809090e16c4463e09f
nharenberg <n.harenberg@live.com>
2016-08-06 11:54:36 -0700

    basic app.js frame

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-const express = require("express")
+//CONSTANTS
+const PORT = 8000;
+
+//REQUIRES
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+// APP DECLARATION
+const app = express();
+
+
+//GENERAL MIDDLEWARE
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({encoded: true}));
+app.use(express.static("public"));
+
+//ROUTES
+
+//SERVER LISTEN
+app.listen(PORT, err => {
+  console.log(err || `Server listening on port ${PORT}`);
+})
